97a05bf1f373dd0b8a3a180baa771843421ca645
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-02-10 22:38:15 -0500

    made setup for books to be avalible (true or false bol)

--- assignments/books.java
+++ assignments/books.java
@@ -7,22 +7,22 @@ public class books {
     String bookAuthorName1 = "carl paul";
     String bookName1 = "the handy cook book";
     String book1Id = "1234567899";
-
+    boolean book1 = true;
     //book2 setup
     String bookAuthorName2 = "victor jest";
     String bookName2= "how to manage family";
     String book2Id = "2345678910";
-
+    boolean book2 = true;
     //book3 setup
     String bookAuthorName3 = "sasha tifto";
     String bookName3 = "trains and transit";
     String book3Id = "3456789101";
-
+    boolean book3 = true;
     //book4 setup
     String bookAuthorName4 = "andre invecee";
     String bookName4= "im lonely";
     String book4Id = "4567891011";
-
+    boolean book4 = true;
 
     
     void displayBookInfo() {
@@ -32,17 +32,17 @@ public class books {
         System.out.println("title: " + bookName1);
         System.out.println("Id: " + book1Id);
         //diplay book 2
-        System.out.println("\n---books---");
+
         System.out.println("author: " + bookAuthorName2);
         System.out.println("title: " + bookName2);
         System.out.println("Id: " + book2Id);
         //diplay book 3
-        System.out.println("\n---books---");
+      
         System.out.println("author: " + bookAuthorName3);
         System.out.println("title: " + bookName3);
         System.out.println("Id: " + book3Id);
         //diplay book 4
-        System.out.println("\n---books---");
+   
         System.out.println("author: " + bookAuthorName4);
         System.out.println("title: " + bookName4);
         System.out.println("Id: " + book4Id);
