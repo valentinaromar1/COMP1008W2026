bf4aa2109b3d6b0ea198d1d40739d4719be50319
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-03-10 22:29:32 -0400

    done lab 4

--- labs/StudentValidatorLab.java
+++ labs/StudentValidatorLab.java
@@ -45,13 +45,16 @@ public class StudentValidatorLab {
          System.out.print("Enter student id: ");
 
         String stId = scanner.nextLine(); 
-        
- 
  
         // STEP 4:
         // Validate format using regex
- 
- 
+        String stIdregex = ("\\d+");
+        
+        if(stId.matches(stIdregex)){
+            System.out.println("Student ID: S-" + stId);
+        }else{
+            System.out.println("this email is invaild");
+        }
  
  
         // =====================================================
@@ -73,7 +76,7 @@ public class StudentValidatorLab {
         String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9+_.-]+$";
  
         if(email.matches(emailRegex)){
-            System.out.println("this email is vaild");
+            System.out.println("email:" + email);
         }else{
             System.out.println("this email is invaild");
         }
@@ -87,26 +90,40 @@ public class StudentValidatorLab {
  
         // STEP 7:
         // Ask user to enter a sentence
- 
+         System.out.print("Enter sentance: ");
+
+        String sentence = scanner.nextLine(); 
  
  
  
         // STEP 8:
         // Count vowels using Character class
- 
- 
- 
- 
+            int count = 0;
+
+            char characterTest = Character.toLowerCase(sentence.charAt(0));
+
+            // Check if the character is a vowel
+            if (characterTest == 'a' || characterTest == 'e' || characterTest  == 'i' || characterTest  == 'o' || characterTest  == 'u') {
+                count = count + 1;
+
+            }
+
+            System.out.println("the amount of vowels in the sentance is" + count);
+            
         // STEP 9:
         // Reverse sentence using StringBuilder
  
+         
+        // Replace all digits with "*"
+        
+        String replaced = sentence.replaceAll("\\d","*");
  
+ 
+        System.out.println(replaced);
  
  
         // STEP 10:
         // Replace all digits in sentence with '*'
- 
- 
  
  
         // =====================================================
@@ -118,10 +135,15 @@ public class StudentValidatorLab {
         // Create string: "Math,Science,Java,English"
         // Split using comma
         // Print each course
+        String subjects = "Math,Science,Java,English";
  
- 
- 
- 
+       
+        String[] tokens = subjects.split(",");
+        
+
+        for(String subject : tokens){
+            System.out.println(subjects);
+        }
         // =====================================================
         // PART 6 — PATTERN & MATCHER
         // =====================================================
